353b1483b288555fea6289c17f418ddd667ed569
Shanbo Zhang <EscapistArcadia@outlook.com>
2025-05-06 21:22:59 +0000

    Updates scripts for dependencies and cross-platform compilers, for Ubuntu.

--- scripts/install_cross_platform.sh
+++ scripts/install_cross_platform.sh
@@ -1,57 +1,189 @@
-#!/bin/bash
-# set -x
-# set -e
+#!/usr/bin/env bash
 
-arch=$(uname -m)
-gcc_ver=14.2.0
-binutils_ver=16.2
-gdb_ver=2.44
+set -e
+set -u
+set -o pipefail
 
-valid_archs=("x86_64" "aarch64" "riscv64" "i386" "i686" "arm" "arm64")
-color_begin="\e[1;32m"
-color_end="\e[0m"
-echo -e "${color_begin}Installing cross-compilation toolchain for ${arch}${color_end}"  
+sudo -v
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 --arch <arch> [--gcc_ver <version>] [--binutils_ver <version>] [--gdb_ver <version>]"
-    echo "Supported architectures: ${valid_archs[*]}"
-    exit 1
-fi
+# homepage: https://ftp.gnu.org/gnu/
+
+ver_binutils=2.44 # TODO: always checks latest version
+ver_gcc=15.1.0
+ver_gdb=16.3
+
+link_binutils=https://ftp.gnu.org/gnu/binutils/binutils-${ver_binutils}.tar.xz
+link_gcc=https://ftp.gnu.org/gnu/gcc/gcc-${ver_gcc}/gcc-${ver_gcc}.tar.xz
+link_gdb=https://ftp.gnu.org/gnu/gdb/gdb-${ver_gdb}.tar.xz
+
+install_binutils=false
+install_gcc=false
+install_gdb=false
+
+build_dir=$(realpath ./build)
+keep_build=false
+
+archs=()
+# arch="$(uname -m)"
 
 while [[ $# -gt 0 ]]; do
-    case $1 in
+    key="$1"
+    case $key in
+        --gcc)
+            install_gcc=true
+            shift
+            ;;
+        --gdb)
+            install_gdb=true
+            shift
+            ;;
+        --binutils)
+            install_binutils=true
+            shift
+            ;;
         --arch)
-            if [[ -n $2 ]]; then
-                arch=$2
+            shift
+            while [[ $# -gt 0 && ! $1 =~ ^-- ]]; do
+                archs+=("$1")
+                shift
+            done
+            ;;
+        # --arch)
+        #     if [[ $# -gt 1 ]]; then
+        #         arch="$2"
+        #         shift
+        #     else
+        #         echo "Error: --arch option requires an argument."
+        #         exit 1
+        #     fi
+        #     shift
+        #     ;;
+        --build)
+            if [[ $# -gt 1 ]]; then
+                build_dir=$(realpath "$2")
                 shift
             else
-                echo "Error: --arch requires an argument."
+                echo "Error: --build option requires an argument."
                 exit 1
             fi
-            ;;
-        --gcc_ver)
-            gcc_ver=$2
             shift
             ;;
-        --binutils_ver)
-            binutils_ver=$2
+        --keep-build)
+            keep_build=true
             shift
             ;;
-        --gdb_ver)
-            gdb_ver=$2
-            shift
+        --help)
+            echo "Usage: $0 [OPTIONS]"
+            echo "Options:"
+            echo "  --gcc          Install GCC"
+            echo "  --gdb          Install GDB"
+            echo "  --binutils     Install Binutils"
+            echo "  --arch <arch>  Specify (multiple) architecture (e.g., x86_64, arm, etc.)"
+            echo "  --build <dir>  Specify build directory (default: ./build)"
+            echo "  --keep-build   Keep build directory after installation"
+            echo "  --help         Show this help message"
+            exit 0
             ;;
         *)
-            echo "Error: Invalid option $1"
+            echo "Error: Unknown option: $key"
+            echo "Use --help for usage information."
             exit 1
             ;;
     esac
-    shift
 done
 
-install_gcc() {
-    
-    # Add your installation commands here
-}
+if [[ $install_binutils == false && $install_gcc == false && $install_gdb == false ]]; then
+    echo "Error: At least one of --gcc, --gdb, or --binutils must be specified."
+    exit 1
+fi
 
-install_gcc
+if [[ -d $build_dir && "$(ls -A $build_dir)" ]]; then
+    echo "Error: Build directory $build_dir is not empty."
+    exit 1
+elif [[ ! -d $build_dir ]]; then
+    mkdir -p "$build_dir"
+fi
+
+cd "$build_dir"
+if [[ $install_binutils == true ]]; then
+    mkdir -p build_binutils
+    cd build_binutils
+    wget "$link_binutils" -O binutils.tar.xz
+    
+    mkdir -p build
+    mkdir -p binutils
+    tar Jxf binutils.tar.xz -C binutils --strip-components=1
+
+    cd build
+
+    for arch in "${archs[@]}"; do
+        ../binutils/configure --prefix=/usr/local --target=${arch}-elf --program-prefix=${arch}-elf-
+        make -j$(nproc) all
+        sudo make install
+        make distclean
+    done
+    cd ..
+
+    cd ..
+fi
+
+if [[ $install_gcc == true ]]; then
+    mkdir -p build_gcc
+    cd build_gcc
+    wget "$link_gcc" -O gcc.tar.xz
+    
+    mkdir -p build
+    mkdir -p gcc
+    tar Jxf gcc.tar.xz -C gcc --strip-components=1
+
+    cd build
+    for arch in "${archs[@]}"; do
+        ../gcc/configure \
+            --enable-languages=c \
+            --without-headers \
+            --disable-nls \
+            --disable-libssp \
+            --disable-libmudflap \
+            --disable-multilib \
+            --disable-shared \
+            --disable-threads \
+            --disable-libgomp \
+            --disable-libquadmath \
+            --prefix=/usr/local --target=${arch}-elf --program-prefix=${arch}-elf- 
+        make -j$(nproc) all-gcc all-target-libgcc
+        sudo make install-gcc install-target-libgcc
+        make distclean
+    done
+    cd ..
+
+    cd ..
+fi
+
+if [[ $install_gdb == true ]]; then
+    mkdir -p build_gdb
+    cd build_gdb
+    wget "$link_gdb" -O gdb.tar.xz
+    
+    mkdir -p build
+    mkdir -p gdb
+    tar Jxf gdb.tar.xz -C gdb --strip-components=1
+
+    cd build
+
+    for arch in "${archs[@]}"; do
+        ../gdb/configure --prefix=/usr/local --target=${arch}-elf --program-prefix=${arch}-elf-
+        make -j$(nproc) all
+        sudo make install
+        make distclean
+    done
+    cd ..
+
+    cd ..
+fi
+
+cd ..
+if [[ $keep_build == false ]]; then
+    rm -rf $build_dir
+fi
+
+# references: https://stackoverflow.com/questions/66355305/how-do-i-install-x86-64-elf-ld-on-ubuntu-20-04
---
+++ scripts/install_dependency.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+sudo -v
+
+sudo apt update
+sudo apt install -y build-essential make gcc g++
+sudo apt install -y wget
+sudo apt install -y qemu-system
+sudo apt install -y libgmp-dev libmpfr-dev libmpc-dev
